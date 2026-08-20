bf5dcf4f0f32597b5729acdcf7d5cc5d04ffc452
CoderYellow <CoderYellow@hotmail.com>
2026-08-20 20:48:30 +0800

    chore(examples): bump `bigdata-test` plugin to v0.1.42 across modules

--- examples/kafka-hudi-hms/build.gradle.kts
+++ examples/kafka-hudi-hms/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     application
     kotlin("jvm") version "2.2.21"
     id("org.openprojectx.spark.platform") version "0.1.42"
-    id("org.openprojectx.bigdata-test") version "0.1.40"
+    id("org.openprojectx.bigdata-test") version "0.1.42"
 }
 
 kotlin {
